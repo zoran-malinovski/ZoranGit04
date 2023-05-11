8eb0f076f708be0ed932f57da81b78f4b6322f90
Zoran <z.malinovski79@gmail.com>
2023-05-11 09:34:30 +0200

    added Test01

--- src/test/java/Git04Test01.java
+++ src/test/java/Git04Test01.java
@@ -1,2 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Git04Test01 {
+
+    @Test
+    public void Test01(){
+
+        System.out.println("Prv test");
+
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "C:\\Chrome Drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+    }
 }
